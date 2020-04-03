43161dcf0f3e5a5c855f03d4e3047d26cd18f954
John Evans <john.wes.evans@gmail.com>
2020-04-03 18:05:43 -0500

    Initial Commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
