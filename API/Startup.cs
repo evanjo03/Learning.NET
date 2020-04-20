25eab71d73fa935a6a3dbe7cec0b7053a97dc177
John Evans <john.wes.evans@gmail.com>
2020-04-20 18:42:00 -0500

    Pagination, Filtering, Sorting

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,13 @@ namespace API
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyOrigin().AllowAnyHeader().WithOrigins("https://localhost:4200");
+                });
+            });
 
 
         }
@@ -50,6 +57,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
